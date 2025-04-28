f74b4cb352a27cbae854d45f90018c6bb471cb7c
Mrlong101092 <mrlong01012020@gmail.com>
2025-04-28 21:10:18 +0700

    05: Neon and Drizzle Setup

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,16 @@
 "use client";
-import { cn } from "@/lib/utils";
+import { cn, getInitials } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Session } from "next-auth";
 
-const Header = () => {
+const Header = ({ session }: { session: Session }) => {
   const pathname = usePathname();
   return (
     <header className="my-10 flex justify-between gap-5">
       <Link href="/">
-      <img 
-        src="/icons/logo.svg" 
-        alt="logo" 
-        width={40}
-        height={40}
-      />
+        <img src="/icons/logo.svg" alt="logo" width={40} height={40} />
       </Link>
 
       <ul className="flex flex-row items-center gap-8">
@@ -26,6 +23,16 @@ const Header = () => {
             )}
           >
             Library
+          </Link>
+        </li>
+
+        <li>
+          <Link href="/my-profile">
+            <Avatar>
+              <AvatarFallback className="bg-amber-100">
+                {getInitials(session?.user?.name || 'IN')}
+              </AvatarFallback>
+            </Avatar>
           </Link>
         </li>
       </ul>
